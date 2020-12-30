b0cd0c43a670866fb06b2abbfc938ae014c70669
Leelalu <leelalu821@gmail.com>
2020-12-30 06:18:30 -0700

    Add modkey to utilize floating windows

--- xmonad.hs
+++ xmonad.hs
@@ -10,7 +10,7 @@ import Data.Monoid
 import Data.Map
 import System.Exit
 
--- Main application applying values to Xconf record
+-- Main func applying values to Xconf record
 main = do xmonad $ def{
  -- Mouse settings
  focusFollowsMouse = True,
@@ -24,29 +24,34 @@ main = do xmonad $ def{
  -- Workspaces
  XMonad.workspaces = ["Main", "Rand"],
  -- Apply keys
- XMonad.keys = \conf -> Data.Map.fromList $ [
+ modMask = mod4Mask,
+ XMonad.keys = \conf@(XConfig {XMonad.modMask = modm}) -> Data.Map.fromList $ [
   -- launch term
-  ((mod4Mask, xK_t) , spawn "alacritty")
+    ((modm, xK_t), spawn "alacritty")
   -- dmenu
-  , ((mod4Mask,xK_x) , spawn "dmenu_run -nb '#1e1e1e' -sf '#ffffff' -sb '#663399' -nf '#ffffff' -p 'run:'")
+  , ((modm, xK_x), spawn "dmenu_run -nb '#1e1e1e' -sf '#ffffff' -sb '#663399' -nf '#ffffff' -p 'run:'")
+  -- dmenu status
+  , ((modm, xK_d), spawn "~/bin/dmenustatus")
   -- i3lock
-  , ((mod4Mask , xK_z) , spawn "i3lock --blur=1 --color=111111 --insidevercolor=D8BFD8 --insidewrongcolor=8B7B8B    --insidecolor=BF55EC --ringvercolor=E066FF --ringwrongcolor=816687 --ringcolor=663399 --keyhlcolor=8B3A62")
+  , ((modm , xK_z), spawn "i3lock --blur=1 --color=111111 --insidevercolor=D8BFD8 --insidewrongcolor=8B7B8B --insidecolor=BF55EC --ringvercolor=E066FF --ringwrongcolor=816687 --ringcolor=663399 --keyhlcolor=8B3A62")
   -- close window
-  , ((mod4Mask, xK_0) , kill)
+  , ((modm, xK_0), kill)
   -- rotate through layouts
-  , ((mod4Mask, xK_space )  , sendMessage NextLayout)
+  , ((modm, xK_space ), sendMessage NextLayout)
+  -- send floating window to tiling
+  , ((modm, xK_l), withFocused $ windows . XMonad.StackSet.sink)
   -- Move focus to  next window/workspace
-  , ((mod4Mask, xK_o) , windows XMonad.StackSet.focusDown)
-  , ((mod4Mask, xK_b) , nextWS)
+  , ((modm, xK_o), windows XMonad.StackSet.focusDown)
+  , ((modm, xK_b), nextWS)
   -- Swap  focused window with next window/to next workspace
-  , ((mod4Mask .|. shiftMask, xK_o) , windows XMonad.StackSet.swapDown)
-  , ((mod4Mask .|. shiftMask, xK_b), shiftToNext >> nextWS)
+  , ((modm .|. shiftMask, xK_o), windows XMonad.StackSet.swapDown)
+  , ((modm .|. shiftMask, xK_b), shiftToNext >> nextWS)
   -- Expand/Shrink master area
-  , ((mod4Mask, xK_m) , sendMessage Expand)
-  , ((mod4Mask .|. shiftMask,xK_m) , sendMessage Shrink)
+  , ((modm, xK_m) , sendMessage Expand)
+  , ((modm .|. shiftMask,xK_m), sendMessage Shrink)
   -- Increase/Deincrement windows master area
-  , ((mod4Mask    , xK_n ) , sendMessage (IncMasterN 1))
-  , ((mod4Mask .|. shiftMask   , xK_n) , sendMessage (IncMasterN (-1)))
+  , ((modm, xK_n ) , sendMessage (IncMasterN 1))
+  , ((modm .|. shiftMask, xK_n) , sendMessage (IncMasterN (-1)))
   -- Quit/Restart xmonad
-  , ((mod4Mask    , xK_s) , spawn "xmonad --recompile; xmonad --restart")
-  , ((mod4Mask .|. shiftMask, xK_q) , io (exitWith ExitSuccess))]}
+  , ((modm, xK_s) , spawn "xmonad --recompile; xmonad --restart")
+  , ((modm .|. shiftMask, xK_q) , io (exitWith ExitSuccess))]}

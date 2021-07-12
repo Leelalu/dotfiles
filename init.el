a8a0ac1f274e5f28475b1fecf408b2f73a235068
Leelalu <leelalu821@gmail.com>
2021-07-12 14:26:35 -0600

    Fixes emacs opacity

--- init.el
+++ init.el
@@ -177,8 +177,8 @@
  (set-foreground-color "#FFFFFF")
  (set-background-color "#222222")
  ; Opacity
- (set-frame-parameter (selected-frame) 'alpha '(85 . 85))
- (add-to-list 'default-frame-alist '(alpha . (85 . 85)))
+ (set-frame-parameter (selected-frame) 'alpha '(70 . 70))
+ (add-to-list 'default-frame-alist '(alpha . (70 . 70)))
  ; Dim inactive windows
  (auto-dim-other-buffers-mode t)
  ; Turn off bell

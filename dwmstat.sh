ec683a2abf956e05211fe42f5106abfbeb42b64e
Leelalu <leelalu821@gmail.com>
2022-06-17 19:58:46 -0600

    Replace irregular tabs with spaces

--- dwmstat.sh
+++ dwmstat.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 ljust(){
-	strOutput=$1
-	strCount="${#1}"
-	strOffset=$(expr $2-$strCount | bc)
+ strOutput=$1
+ strCount="${#1}"
+ strOffset=$(expr $2-$strCount | bc)
 
-	while [[ 1 -le $strOffset ]]
-	do
-			strOutput="${strOutput} "
-			strOffset=$(( $strOffset-1 ))
-	done
+ while [[ 1 -le $strOffset ]]
+ do
+  strOutput="${strOutput} "
+  strOffset=$(( $strOffset-1 ))
+ done
 
-	echo "$strOutput"
+ echo "$strOutput"
 }
 
 retreiveNewTime(){
-	while
-			NEWTIME=$(date "+%H:%M:%S %Z")
-			[ "$1" == "$NEWTIME" ]
-	do true; done
+ while
+  NEWTIME=$(date "+%H:%M:%S %Z")
+  [ "$1" == "$NEWTIME" ]
+ do true; done
 
-	echo $NEWTIME
+ echo $NEWTIME
 }
 
-	xsetroot -name "| $CPU $TEMP $RAM   $BATT $VOL $BRIGHTNESS   $DATE $TIME "
 while true; do
-  CPU=$(ljust $(top -bn1 | grep Cpu | awk '{print $2}')% 5)
-	TEMP=$(ljust $(sensors | sed  -n '0,/^Pack/s/\..*//p' | grep -Eo '[0-9]+$')C 4)
-  RAMTOTAL=$(vmstat -s -S m | grep 'total memory' | awk '{print $1;}')
-	RAMUSED=$(vmstat -s -S m | grep 'used memory' | awk '{print $1;}')
-	RAM=$(ljust "$RAMTOTAL/$RAMUSED" 9)
+ CPU=$(ljust $(top -bn1 | grep Cpu | awk '{print $2}')% 5)
+ TEMP=$(ljust $(sensors |  grep temp |awk  '{print $2}' |  grep -Eo '[0-9]+' | head -n1)C 4)
 
-  BATT=$(ljust $(acpi | awk 'NF>1{print $NF}') 4)
-  VOL=$(ljust $(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }') 4)
-	XBRIGHTNESS=$(xrandr --verbose | grep Bright | cut -d " " -f2 | head -n 1)
-  BRIGHTNESS=盛$(ljust $(expr $XBRIGHTNESS*100 | bc | cut -d "." -f1)% 4)
+ RAMTOTAL=$(vmstat -s -S m | grep 'total memory' | awk '{print $1;}')
+ RAMUSED=$(vmstat -s -S m | grep 'used memory' | awk '{print $1;}')
+ RAM=$(ljust "$RAMTOTAL/$RAMUSED" 9)
 
-  DATE=$(date "+%d-%m-%y")
-  TIME=$(retreiveNewTime $OLDTIME)
+ BATT=$(ljust $(upower -i `upower -e | grep 'BAT'` | grep percent |  awk  '{print $2}') 4)
+ VOL=$(ljust $(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }') 4)
+ XBRIGHTNESS=$(xrandr --verbose | grep Bright | cut -d " " -f2 | head -n 1)
+ BRIGHTNESS=盛$(ljust $(expr $XBRIGHTNESS*100 | bc | cut -d "." -f1)% 4)
 
-	xsetroot -name "| $CPU $TEMP $RAM   $BATT $VOL $BRIGHTNESS   $DATE $TIME "
-	OLDTIME=$TIME
+ DATE=$(date "+%d-%m-%y")
+ TIME=$(retreiveNewTime $OLDTIME)
+
+ xsetroot -name "| $CPU $TEMP $RAM   $BATT $VOL $BRIGHTNESS   $DATE $TIME "
+ OLDTIME=$TIME
 done
